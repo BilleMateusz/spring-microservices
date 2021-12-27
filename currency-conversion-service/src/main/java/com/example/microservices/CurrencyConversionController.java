c5d9074e927a7796c28b9184422454626942bf44
mmaciaszek <mateusz.maciaszek@billennium.com>
2021-12-27 15:26:07 +0100

    call CurrencyExchangeService from CurrencyConversionController (142)

--- currency-conversion-service/src/main/java/com/example/microservices/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/microservices/CurrencyConversionController.java
@@ -1,15 +1,21 @@
 package com.example.microservices;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RequiredArgsConstructor
 @RestController
 public class CurrencyConversionController {
+
+    private static final boolean USE_WEBCLIENT = true;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{q}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -17,10 +23,31 @@ public class CurrencyConversionController {
             @PathVariable String to,
             @PathVariable BigDecimal q
     ) {
-        return new CurrencyConversion(10001L, from, to, q,
-                BigDecimal.ONE,
-                BigDecimal.ONE,
-                ""
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        final String currencyExchangeUri = "http://localhost:8000/currency-exchange/from/{from}/to/{to}";
+
+        CurrencyConversion currencyConversion;
+        if (USE_WEBCLIENT) {
+            currencyConversion = WebClient.builder()
+                    .baseUrl(currencyExchangeUri)
+                    .defaultUriVariables(uriVariables)
+                    .build()
+                    .get()
+                    .retrieve()
+                    .bodyToMono(CurrencyConversion.class)
+                    .block();
+        } else {
+            ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+                    currencyExchangeUri, CurrencyConversion.class, uriVariables);
+            currencyConversion = responseEntity.getBody();
+        }
+
+        return new CurrencyConversion(currencyConversion.getId(), from, to, q,
+                currencyConversion.getConversionMultiple(),
+                q.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()
         );
     }
 }
