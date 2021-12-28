42cd6444fe77d3b6fb83c5f6a3b17e7f0fe5cae2
mmaciaszek <mateusz.maciaszek@billennium.com>
2021-12-28 11:27:51 +0100

    make feign use service name instead of url (148)

--- currency-conversion-service/src/main/java/com/example/microservices/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/example/microservices/CurrencyExchangeProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange") //url property not needed when name is registered with naming server
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
