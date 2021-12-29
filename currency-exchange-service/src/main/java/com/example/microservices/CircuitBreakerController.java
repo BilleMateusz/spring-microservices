b51f178d0d791a5955656ae2b17ce5fbe9c9e2ea
mmaciaszek <mateusz.maciaszek@billennium.com>
2021-12-29 14:02:21 +0100

    add retry and fallback with resilience4j (155)

--- currency-exchange-service/src/main/java/com/example/microservices/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/CircuitBreakerController.java
@@ -1,13 +1,26 @@
 package com.example.microservices;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi() {
-        return "Sample API";
+        log.info("Sample Api call received");
+        ResponseEntity<String> r = new RestTemplate().getForEntity("http://localhost:8080/some-url", String.class);
+        return r.getBody();
     }
+
+    private String hardcodedResponse(Exception e) {
+        return "fallback-response";
+    }
+
 }
