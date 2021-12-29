09ee2c206b8e3146fc1328ef0a67bc1a9efbaf13
mmaciaszek <mateusz.maciaszek@billennium.com>
2021-12-29 15:19:16 +0100

    add rateLimiter and bulkhead with resilience4j (159)

--- currency-exchange-service/src/main/java/com/example/microservices/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/CircuitBreakerController.java
@@ -1,11 +1,10 @@
 package com.example.microservices;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RestController
@@ -14,15 +13,21 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
     //circuit breaker can be in closed(low failure rate), open(high failure rate) of half-open state
-    @CircuitBreaker(name="sample-api", fallbackMethod="hardcodedResponse")
+//    @CircuitBreaker(name="sample-api", fallbackMethod="hardcodedResponse")
+    @RateLimiter(name = "default", fallbackMethod = "rateLimiterFallback")
+    @Bulkhead(name = "default")
     public String sampleApi() {
         log.info("Sample Api call received");
-        ResponseEntity<String> r = new RestTemplate().getForEntity("http://localhost:8080/some-url", String.class);
-        return r.getBody();
+        return "sample-api";
     }
 
     private String hardcodedResponse(Exception e) {
         return "fallback-response";
+    }
+
+    private String rateLimiterFallback(Exception e) {
+        log.info(e.getMessage());
+        return "rate limiter fallback";
     }
 
 }
