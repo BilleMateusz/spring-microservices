f430f9dfa6da7e75505b6d07619c8ba7b2adfa34
mmaciaszek <mateusz.maciaszek@billennium.com>
2021-12-29 14:04:03 +0100

    add circuit breaker with resilience4j (157)

--- currency-exchange-service/src/main/java/com/example/microservices/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/CircuitBreakerController.java
@@ -1,6 +1,6 @@
 package com.example.microservices;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +12,9 @@ import org.springframework.web.client.RestTemplate;
 public class CircuitBreakerController {
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    //circuit breaker can be in closed(low failure rate), open(high failure rate) of half-open state
+    @CircuitBreaker(name="sample-api", fallbackMethod="hardcodedResponse")
     public String sampleApi() {
         log.info("Sample Api call received");
         ResponseEntity<String> r = new RestTemplate().getForEntity("http://localhost:8080/some-url", String.class);
